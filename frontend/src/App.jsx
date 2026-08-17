fc481908e5ac56e46cc9b9ee46c354a5a2f314fc
Yashna <yashna@my.yorku.ca>
2026-08-17 19:31:48 -0400

    imported all functions for the root

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { api } from './api.js';
+import Sidebar from './components/Sidebar.jsx';
+import PlayerHeader from './components/PlayerHeader.jsx';
+import StatCards from './components/StatCards.jsx';
+import ShotChart from './components/ShotChart.jsx';
+import HeatTimeline from './components/HeatTimeline.jsx';
+import HotZoneOverlay from './components/HotZoneOverlay.jsx';
+import ComparePanel from './components/ComparePanel.jsx';
+
+export default function App()
+{
+    const [players, ]
+}
